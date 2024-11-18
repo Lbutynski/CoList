10ad5168cc23fa1429d031479d9407fdfc9336b8
Lbutynski <lucas.butynski@supdevinci-edu.fr>
2024-11-18 16:17:27 +0100

    change storage from temporary to mongo

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const http = require("http");
+const { default: mongoose } = require("mongoose").set("debug", true);
 const { Server } = require("socket.io");
-
+require("dotenv").config();
 const SERVER_PORT = 3001;
 const app = express();
-
+const MONGO_URI = process.env.MONGO_URI;
+(async () => {
+  await mongoose.connect(MONGO_URI);
+})();
+const listItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  items: [String],
+});
+const ListItem = new mongoose.model("ListItem", listItemSchema);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
@@ -13,23 +22,43 @@ const io = new Server(server, {
     credentials: true,
   },
 });
+const retrieveList = async (id) => {
+  const list = await ListItem.findById(id);
+  return list ? list.items : [];
+};
 const sharedList = ["test", "re"];
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("Un utilisateur est connecté :", socket.id);
-  socket.emit("updateList", sharedList);
+  const listId = "673b5242259d4a54b7caf04e";
+  let list = await retrieveList(listId);
+  socket.emit("updateList", list);
 
-  socket.on("addItem", (item) => {
-    sharedList.push(item);
-    io.emit("updateList", sharedList);
+  socket.on("addItem", async (item) => {
+    await ListItem.findByIdAndUpdate(
+      listId,
+      { $push: { items: item } },
+      { new: true }
+    );
+    list = await retrieveList(listId);
+    io.emit("updateList", list);
   });
-  socket.on("removeItem", (index) => {
-    sharedList.splice(index, 1);
-    io.emit("updateList", sharedList);
+  socket.on("removeItem", async (index) => {
+    const currentList = await ListItem.findById(listId);
+    if (currentList && currentList.items[index]) {
+      currentList.items.splice(index, 1);
+      await currentList.save();
+      list = await retrieveList(listId);
+      io.emit("updateList", list);
+    }
   });
-  socket.on("changeItem", (index, change) => {
-    sharedList[index] = change;
-    console.log(sharedList);
-    io.emit("updateList", sharedList);
+  socket.on("changeItem", async (index, newItem) => {
+    const currentList = await ListItem.findById(listId);
+    if (currentList && currentList.items[index]) {
+      currentList.items[index] = newItem;
+      await currentList.save();
+      list = await retrieveList(listId);
+      io.emit("updateList", list);
+    }
   });
   socket.on("disconnect", () => {
     console.log("Un utilisateur est déconnecté");
